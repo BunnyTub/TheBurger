e083833b08b0d42271eb01bafc39c9cde65e293d
BunnyTub <gadiel047@gmail.com>
2026-02-06 05:11:39 -0500

    The Burger

--- TheBurger/Properties/AssemblyInfo.cs
+++ TheBurger/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyCoolImage")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("The Burger")]
+[assembly: AssemblyDescription("It is a burger. It is very enjoyable.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyCoolImage")]
+[assembly: AssemblyProduct("The Burger")]
 [assembly: AssemblyCopyright("BunnyTub")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.3.7")]
+[assembly: AssemblyFileVersion("1.3.3.7")]
